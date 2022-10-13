45e7e63fa7768eaf3bf3449dd0ae2d91f12a995c
Pavel Kogut <kogut.kogut1@mail.ru>
2022-10-13 19:48:44 +0500

    A few edits

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('Everything is ready!');
+    const scrollLinks = document.querySelectorAll('li>a');
+    
+     for (const scrollLink of scrollLinks) {
+       scrollLink.addEventListener('click', event => {
+         event.preventDefault();
+         const id = scrollLink.getAttribute('href');
+         document.querySelector(id).scrollIntoView({
+           behavior: 'smooth',
+           block: 'start',
+           });
+       });
+      }
+  
+    let arrowUp = document.querySelector('.up');
+
+    arrowUp.addEventListener('click', () => {
+      document.querySelector('body').scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+    
